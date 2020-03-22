8c8ceab6e55d41996cee0ec493cca9c0c7acae16
KIM JUNSUNG <naber9065@naver.com>
2020-03-22 23:11:57 +0900

    프로그래머스 Lv2 쇠막대기

---
+++ src/yahait/level2/Stick.java
@@ -0,0 +1,28 @@
+package src.yahait.level2;
+
+import java.util.Stack;
+
+public class Stick {
+	public static void main(String[] args){
+		Stick obj = new Stick();
+		int a = obj.solution("()(((()())(())()))(())");
+		System.out.println(a);
+	}
+	
+	public int solution(String arrangement) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < arrangement.length(); i++) {
+			if (i < arrangement.length()-1 && arrangement.charAt(i) == '(' && arrangement.charAt(i+1) == ')') {
+				i++;
+				answer += stack.size();
+			}else if (arrangement.charAt(i) == '(') {
+				stack.add(1);
+			}else if (arrangement.charAt(i) == ')') {
+				stack.remove(stack.size() - 1);
+				answer++;
+			}
+		}
+		return answer;
+	}
+}
